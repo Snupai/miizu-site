5d1dc1bd0fdeadc45f07e0f9cd1c30dfaa293d32
Snupai (Nya~) <nya@snupai.me>
2025-01-27 21:02:18 +0100

    Update search route to use nullish coalescing operator

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { type NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const searchQuery = request.nextUrl.searchParams.get('q')?.toLowerCase() || '';
+  const searchQuery = request.nextUrl.searchParams.get('q')?.toLowerCase() ?? '';
 
   const pages = [
     {
