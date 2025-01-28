8c84eb4a46a7c7e3538cc6e15e636167fbecd98c
Snupai (Nya~) <nya@snupai.me>
2025-01-28 13:00:23 +0100

    Migrate Navigation logo to Next.js Image component

--- src/app/_components/Navigation.tsx
+++ src/app/_components/Navigation.tsx
@@ -1,16 +1,19 @@
 'use client'
 
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navigation() {
   return (
     <div className="fixed top-2 left-1/2 -translate-x-1/2 z-50">
       <Link href="/">
         <div className="bg-black w-11 h-11 rounded-full flex items-center justify-center cursor-pointer">
-          <img
+          <Image
             src="/images/sentimental.png"
             alt="Sentimental"
-            className="w-7 h-7 object-contain"
+            width={28}
+            height={28}
+            className="object-contain"
           />
         </div>
       </Link>
